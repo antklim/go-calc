46f4a71aaacd943b160f2bf08cdded035d25d91d
antklim <antklim@gmail.com>
2020-11-17 16:51:25 +1100

    Operation validation added

---
+++ validation.go
@@ -0,0 +1,65 @@
+package calc
+
+import "errors"
+
+type Validation func(args ...float64) error
+
+func commonValidation(args ...float64) error {
+	if args == nil {
+		return errors.New("arguments list cannot be nil")
+	}
+
+	return nil
+}
+
+func minOneOperandValidation(args ...float64) error {
+	if err := commonValidation(args...); err != nil {
+		return err
+	}
+
+	if len(args) < 1 {
+		return errors.New("arguments list should have at least one element")
+	}
+
+	return nil
+}
+
+func minTwoOperandsValidation(args ...float64) error {
+	if err := commonValidation(args...); err != nil {
+		return err
+	}
+
+	if len(args) < 2 {
+		return errors.New("arguments list should have at least two elements")
+	}
+
+	return nil
+}
+
+var AddValidation Validation = minTwoOperandsValidation
+var SubValidation Validation = minTwoOperandsValidation
+var MulValidation Validation = minTwoOperandsValidation
+
+func DivValidation(args ...float64) error {
+	if err := minTwoOperandsValidation(args...); err != nil {
+		return err
+	}
+
+	if args[1] == 0 {
+		return errors.New("divisor cannot be zero")
+	}
+
+	return nil
+}
+
+func SqrtValidation(args ...float64) error {
+	if err := minOneOperandValidation(args...); err != nil {
+		return err
+	}
+
+	if args[0] < 0 {
+		return errors.New("square root operand cannot be negative")
+	}
+
+	return nil
+}
---
+++ validation_test.go
@@ -0,0 +1,160 @@
+package calc_test
+
+import (
+	"testing"
+
+	"github.com/antklim/go-calc"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestValidations(t *testing.T) {
+	testCases := []struct {
+		desc       string
+		args       []float64
+		validation calc.Validation
+		assert     func(t *testing.T, err error)
+	}{
+		{
+			desc:       "addition validation returns error when arguments list is nil",
+			args:       nil,
+			validation: calc.AddValidation,
+			assert: func(t *testing.T, err error) {
+				assert.EqualError(t, err, "arguments list cannot be nil")
+			},
+		},
+		{
+			desc:       "addition validation returns error when arguments list is empty",
+			args:       []float64{},
+			validation: calc.AddValidation,
+			assert: func(t *testing.T, err error) {
+				assert.EqualError(t, err, "arguments list should have at least two elements")
+			},
+		},
+		{
+			desc:       "addition validation returns nil when arguments list is valid",
+			args:       []float64{1, 2},
+			validation: calc.AddValidation,
+			assert: func(t *testing.T, err error) {
+				assert.NoError(t, err)
+			},
+		},
+		{
+			desc:       "subtraction validation returns error when arguments list is nil",
+			args:       nil,
+			validation: calc.SubValidation,
+			assert: func(t *testing.T, err error) {
+				assert.EqualError(t, err, "arguments list cannot be nil")
+			},
+		},
+		{
+			desc:       "subtraction validation returns error when arguments list is empty",
+			args:       []float64{},
+			validation: calc.SubValidation,
+			assert: func(t *testing.T, err error) {
+				assert.EqualError(t, err, "arguments list should have at least two elements")
+			},
+		},
+		{
+			desc:       "subtraction validation returns nil when arguments list is valid",
+			args:       []float64{1, 2},
+			validation: calc.SubValidation,
+			assert: func(t *testing.T, err error) {
+				assert.NoError(t, err)
+			},
+		},
+		{
+			desc:       "multiplication validation returns error when arguments list is nil",
+			args:       nil,
+			validation: calc.MulValidation,
+			assert: func(t *testing.T, err error) {
+				assert.EqualError(t, err, "arguments list cannot be nil")
+			},
+		},
+		{
+			desc:       "multiplication validation returns error when arguments list is empty",
+			args:       []float64{},
+			validation: calc.MulValidation,
+			assert: func(t *testing.T, err error) {
+				assert.EqualError(t, err, "arguments list should have at least two elements")
+			},
+		},
+		{
+			desc:       "multiplication validation returns nil when arguments list is valid",
+			args:       []float64{1, 2},
+			validation: calc.MulValidation,
+			assert: func(t *testing.T, err error) {
+				assert.NoError(t, err)
+			},
+		},
+		{
+			desc:       "division validation returns error when arguments list is nil",
+			args:       nil,
+			validation: calc.DivValidation,
+			assert: func(t *testing.T, err error) {
+				assert.EqualError(t, err, "arguments list cannot be nil")
+			},
+		},
+		{
+			desc:       "division validation returns error when arguments list is empty",
+			args:       []float64{},
+			validation: calc.DivValidation,
+			assert: func(t *testing.T, err error) {
+				assert.EqualError(t, err, "arguments list should have at least two elements")
+			},
+		},
+		{
+			desc:       "division validation returns error when divisor is zero",
+			args:       []float64{1, 0},
+			validation: calc.DivValidation,
+			assert: func(t *testing.T, err error) {
+				assert.EqualError(t, err, "divisor cannot be zero")
+			},
+		},
+		{
+			desc:       "division validation returns nil when arguments list is valid",
+			args:       []float64{1, 2},
+			validation: calc.DivValidation,
+			assert: func(t *testing.T, err error) {
+				assert.NoError(t, err)
+			},
+		},
+		{
+			desc:       "square root validation returns error when arguments list is nil",
+			args:       nil,
+			validation: calc.SqrtValidation,
+			assert: func(t *testing.T, err error) {
+				assert.EqualError(t, err, "arguments list cannot be nil")
+			},
+		},
+		{
+			desc:       "square root validation returns error when arguments list is empty",
+			args:       []float64{},
+			validation: calc.SqrtValidation,
+			assert: func(t *testing.T, err error) {
+				assert.EqualError(t, err, "arguments list should have at least one element")
+			},
+		},
+		{
+			desc:       "square root validation returns error when operand is negative",
+			args:       []float64{-1},
+			validation: calc.SqrtValidation,
+			assert: func(t *testing.T, err error) {
+				assert.EqualError(t, err, "square root operand cannot be negative")
+			},
+		},
+		{
+			desc:       "square root validation returns nil when arguments list is valid",
+			args:       []float64{1},
+			validation: calc.SqrtValidation,
+			assert: func(t *testing.T, err error) {
+				assert.NoError(t, err)
+			},
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			err := tC.validation(tC.args...)
+			tC.assert(t, err)
+		})
+	}
+}
