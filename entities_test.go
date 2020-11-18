9d2d32a02d377ce6e934a61768477b3c699d3d20
antklim <antklim@gmail.com>
2020-11-18 11:51:51 +1100

    Request entity test added

---
+++ entities_test.go
@@ -0,0 +1,45 @@
+package calc_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/antklim/go-calc"
+)
+
+func TestRequestValidation(t *testing.T) {
+	testCases := []struct {
+		desc   string
+		req    calc.Request
+		assert func(t *testing.T, err error)
+	}{
+		{
+			desc: "returns error when unsupported operation provided",
+			req:  calc.Request{Operation: "foo"},
+			assert: func(t *testing.T, err error) {
+				assert.EqualError(t, err, "unsupported operation foo")
+			},
+		},
+		{
+			desc: "propagates operation validation error",
+			req:  calc.Request{Operation: "add"},
+			assert: func(t *testing.T, err error) {
+				assert.EqualError(t, err, "arguments list cannot be nil")
+			},
+		},
+		{
+			desc: "returns nil when request is valid",
+			req:  calc.Request{Operation: "add", Arguments: []float64{1, 2}},
+			assert: func(t *testing.T, err error) {
+				assert.NoError(t, err)
+			},
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			err := tC.req.Validate()
+			tC.assert(t, err)
+		})
+	}
+}
